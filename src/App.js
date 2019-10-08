5be1517b7a20a00065e5eb96fe804c5a1f064889
theborgh <borghiborghi@gmail.com>
2019-10-08 12:52:48 +0200

    Add useEffect tests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import './App.css';
+import hookActions from './actions/hookActions';
+
+// run automatically
+function reducer(state, action) {
+  switch (action.type) {
+    case 'setSecretWord':
+      return {...state, secretWord: action.payload};
+    default:
+      throw new Error('Invalid action type: ' + action.type);
+  }
+}
 
 function App() {
+  const [state, dispatch] = React.useReducer(reducer, {secretWord: null});
+
+  const setSecretWord = secretWord =>
+    dispatch({type: 'setSecretWord', payload: secretWord});
+
+  React.useEffect(() => hookActions.getSecretWord(setSecretWord), []);
+
   return <div data-test='component-App'></div>;
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {mount} from 'enzyme';
 import {findTagsWithTestAttribute} from '../test/testUtils';
 import App from './App';
 
+import hookActions from './actions/hookActions';
+
+// globally scoped so it can be used in setup()
+const mockGetSecretWord = jest.fn();
+
 const setup = () => {
-  return shallow(<App />);
+  mockGetSecretWord.mockClear(); // so the call count does not accumulate
+  hookActions.getSecretWord = mockGetSecretWord;
+
+  // useEffect not currently called on shallow(). Issue: https://github.com/airbnb/enzyme/issues/2086
+  return mount(<App />);
 };
 
 it('renders without errors', () => {
   const wrapper = setup(<App />);
   expect(findTagsWithTestAttribute(wrapper, 'component-App').length).toBe(1);
+});
+
+describe('getSecretWord calls', () => {
+  it('getSecretWord gets called on App mount', () => {
+    setup();
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
+
+  it('getSecretWord does not get called on App update', () => {
+    const wrapper = setup();
+
+    mockGetSecretWord.mockClear();
+    wrapper.setProps();
+
+    expect(mockGetSecretWord).not.toHaveBeenCalled();
+  });
 });
